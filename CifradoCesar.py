f0a4540bc7aa35d343edb4140889ccbaafa298cd
victorperez200 <78993279+victorperez200@users.noreply.github.com>
2021-05-05 17:26:17 -0400

    Add files via upload

---
+++ CifradoCesar.py
@@ -0,0 +1,34 @@
+﻿# Victor Manuel Perez Vasquez | 2019-8040
+
+
+# Obtenemos mensaje a cifrar desde el usuario
+# llamamos a upper para obtener sólo mayúsculas
+texto = input("Mensaje > ").upper()
+
+# Pedimos al usuario la cantidad de desplazamiento
+n = int(input("Desplazamiento > "))
+
+# Abecedario a utilizar en el cifrado
+abc = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+
+# Variable para guardar mensaje cifrado
+cifrado = ""
+
+# Iteramos por cara letra del mensaje
+for l in texto:
+
+ # Si la letra está en el abecedario se reemplaza
+    if l in abc:
+      pos_letra = abc.index(l)
+
+ # Sumamos para movernos a la derecha del abc
+      nueva_pos = (pos_letra + n) % len(abc)
+      cifrado+= abc[nueva_pos]
+
+    else:
+ # Si no está en el abecedario sólo añadelo
+      cifrado+= l
+
+print("Mensaje cifrado:", cifrado)
+
+ 
